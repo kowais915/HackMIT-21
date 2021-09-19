c0fde9f95fb1d35bcdcb34dbc52c0a7e47664347
kowais915 <kowais915@gmail.com>
2021-09-19 06:45:40 +0500

    installed firebase, removed the two input fields cause I want to use firebase with google auth now :!

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const button = document.getElementById('signIn-button');
-
-
-
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-
-
-
-button.addEventListener('click', ()=>{
-
-alert(userName.value + " " + userEmail.value);
-
-
+document.addEventListener('DOMcontentLoaded', event=>{
+    const app = firebase.app();
+    // console.log(app);
 })
+
+
+// const button = document.getElementById('signIn-button');
+
+
+
+
+
+
+// const userEmail = document.getElementById('email');
+// const userPassword = document.getElementById('password');
+
+
+// button.addEventListener('click', ()=>{
+
+//     auth.createUserWithEmailAndPassword(userEmail.value, userPassword.value);
+//     alert("Done!");
+
+
+// })
+
+
+
+
+
+
